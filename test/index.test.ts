eb3ecce75263e052afdd0dd0cb99625b78744043
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-01 22:31:21 -0500

    updated package.json

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import { getNcVectorData, getToolPathSegmentsData } from '../src/index';
 import { NcCode } from '../src/core/nc/model/nc-code.model';
 
-const GCODE_ARRAY = [
+const GCODE_ARRAY_1 = [
     'N1 G17 G20 G90 G94 G54',
     'N2 G0 Z0.25',
     'N3 X-0.5 Y0.',
@@ -14,9 +14,32 @@ const GCODE_ARRAY = [
     'N10 G01 Z0.1 F5.',
     'N11 G00 X0. Y0. Z0.25'
 ];
+
+const GCODE_ARRAY = [
+    'G21 G17 G90 F100',
+    'M03 S1000',
+    'G00 X5 Y5                 ; point B',
+    'G01 X5 Y5 Z-1             ; point B',
+    'G01 X5 Y15 Z-1            ; point C',
+    'G02 X9 Y19 Z-1 I4 J0      ; point D',
+    'G01 X23 Y19 Z-1           ; point E',
+    'G01 X32 Y5 Z-1            ; point F',
+    'G01 X21 Y5 Z-1            ; point G',
+    'G01 X21 Y8 Z-1            ; point H',
+    'G03 X19 Y10 Z-1 I-2 J0    ; point I',
+    'G01 X13 Y10 Z-1           ; point J',
+    'G03 X11 Y8 Z-1 I0 J-2     ; point K',
+    'G01 X11 Y5 Z-1            ; point L',
+    'G01 X5 Y5 Z-1             ; point B',
+    'G01 X5 Y5 Z0',
+    'G28  X0 Y0',
+    'M05',
+    'M30'
+];
 const INPUT_STRING = GCODE_ARRAY.join('\n');
 
 
+console.log(GCODE_ARRAY_1)
 describe('NC-Toolpath-Generator', () => {
     it('Interpreters', (done: Function) => {
         console.log(INPUT_STRING);
@@ -27,7 +50,7 @@ describe('NC-Toolpath-Generator', () => {
             done();
         });
     });
-    it.only('Toolpath', (done: Function) => {
+    it.skip('Toolpath', (done: Function) => {
         console.log(INPUT_STRING);
         const ncVectorDataPromise: Promise<NcCode[]> = getNcVectorData(INPUT_STRING);
         ncVectorDataPromise.then(async (ncVectorDataList: NcCode[]) => {
